6202a62bbe28f65a86407c056637b2f3f01be792
Anna Bondarchuk <annbondar4uk@gmail.com>
2018-04-02 21:50:21 +0300

    template method

---
+++ Behaviour/TemplateMethod/AppBuilder.php
@@ -0,0 +1,71 @@
+<?php
+
+abstract class Builder
+{
+
+    // Template method
+    final public function build()
+    {
+        $this->test();
+        $this->lint();
+        $this->assemble();
+        $this->deploy();
+    }
+
+    abstract public function test();
+    abstract public function lint();
+    abstract public function assemble();
+    abstract public function deploy();
+}
+
+class AndroidBuilder extends Builder
+{
+    public function test()
+    {
+        echo 'Запуск Android тестов';
+    }
+
+    public function lint()
+    {
+        echo 'Копирование Android кода';
+    }
+
+    public function assemble()
+    {
+        echo 'Android сборка';
+    }
+
+    public function deploy()
+    {
+        echo 'Развертывание сборки на сервере';
+    }
+}
+
+class IosBuilder extends Builder
+{
+    public function test()
+    {
+        echo 'Запуск iOS тестов';
+    }
+
+    public function lint()
+    {
+        echo 'Копирование iOS кода';
+    }
+
+    public function assemble()
+    {
+        echo 'iOS сборка';
+    }
+
+    public function deploy()
+    {
+        echo 'Развертывание сборки на сервере';
+    }
+}
+
+$androidBuilder = new AndroidBuilder();
+$androidBuilder->build();
+echo '</br>';
+$iosBuilder = new IosBuilder();
+$iosBuilder->build();
